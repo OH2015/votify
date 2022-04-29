46b78897d0d12fe8e924ca69843561c866bbcea9
OH2015 <47241990+OH2015@users.noreply.github.com>
2022-04-29 21:40:58 +0900

    s4

--- django/config/settings/production.py
+++ django/config/settings/production.py
@@ -21,6 +21,6 @@ DATABASES = {
 
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 AWS_STORAGE_BUCKET_NAME = 'plbucket'
-AWS_S3_REGION_NAME = 'up-east-1'
+AWS_S3_REGION_NAME = 'us-east-1'
 AWS_ACCESS_KEY_ID = env.get_value('AWS_ACCESS_KEY_ID')
 AWS_SECRET_ACCESS_KEY = env.get_value('AWS_SECRET_ACCESS_KEY')
