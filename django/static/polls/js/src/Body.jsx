3224598ae44ee4f2676f1f7056c2c9795892fb0c
OH2015 <47241990+OH2015@users.noreply.github.com>
2023-02-23 08:02:50 +0900

    delete login button

--- django/static/polls/js/src/Body.jsx
+++ django/static/polls/js/src/Body.jsx
@@ -1,30 +1,76 @@
-import React,{useState, useEffect} from 'react'
-import styled from 'styled-components'
-import Post from './Post'
+import React, { useState, useEffect } from "react";
+import styled from "styled-components";
+import Post from "./Post";
 
 const RootElement = styled.div`
-    max-width: 800px;
+  max-width: 800px;
+  margin: auto;
+  text-align: center;
+`;
+
+const RoundButton = styled.button`
+  width: 50px;
+  height: 50px;
+  border-radius: 50%;
+  background-color: #2196f3;
+  border: none;
+  position: relative;
+  overflow: hidden;
+  cursor: pointer;
+  margin-bottom: 25px;
+`;
+
+const PlusIcon = styled.span`
+  position: absolute;
+  top: 0;
+  left: 0;
+  bottom: 0;
+  right: 0;
+  margin: auto;
+  width: 10px;
+  height: 10px;
+  &::before,
+  &::after {
+    content: "";
+    position: absolute;
+    top: 0;
+    left: 0;
+    bottom: 0;
+    right: 0;
     margin: auto;
-`
+    width: 2px;
+    height: 10px;
+    background-color: white;
+  }
+  &::before {
+    transform: rotate(90deg);
+  }
+  &::after {
+    transform: rotate(180deg);
+  }
+`;
 
 // ボディコンポーネント
 const Body = () => {
-    const [posts,setPosts] = useState([]); //投稿一覧
-    
-    // 初期処理
-    useEffect(() => {
-        const getQuestions = async () => {
-          const res = await axios.get('/api/questions/');
-          setPosts(res.data);
-        }
-        getQuestions();
-    }, []);
+  const [posts, setPosts] = useState([]); //投稿一覧
 
-    return (
-        <RootElement>
-            {posts.map(post => (<Post key={post.id} {...post}/>))}
-        </RootElement>
-    );
-}
+  // 初期処理
+  useEffect(() => {
+    const getQuestions = async () => {
+      const res = await axios.get("/api/questions/");
+      setPosts(res.data);
+    };
+    getQuestions();
+  }, []);
+
+  return (
+    <RootElement>
+      <RoundButton onClick={() => location.href='/create_question'}><PlusIcon></PlusIcon></RoundButton>
+      {posts.map((post) => (
+        <Post key={post.id} {...post} />
+      ))}
+    </RootElement>
+  );
+};
 
 export default Body;
