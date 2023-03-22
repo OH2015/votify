00ada4a3299a5ab68a90b951d024c256de41185d
OH2015 <47241990+OH2015@users.noreply.github.com>
2023-03-22 19:34:34 +0900

    SOCIAL_AUTH_REDIRECT_IS_HTTPS

--- django/config/settings/production.py
+++ django/config/settings/production.py
@@ -44,3 +44,5 @@ CSRF_TRUSTED_ORIGINS = ['https://votify.jp']
 # Google認証の接続情報
 SOCIAL_AUTH_GOOGLE_OAUTH2_KEY = '150637606230-gq5luas1o9mjt6pfcrhrftfpi93neii7.apps.googleusercontent.com'
 SOCIAL_AUTH_GOOGLE_OAUTH2_SECRET = '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'
+# リダイレクトURLにhttpsを使用させる
+SOCIAL_AUTH_REDIRECT_IS_HTTPS = True
