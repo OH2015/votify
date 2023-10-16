f47767bae6aaceae5a980f31644febac17078416
OH2015 <47241990+OH2015@users.noreply.github.com>
2023-10-16 18:04:21 +0900

    add psycopg2

--- django/config/settings/development.py
+++ django/config/settings/development.py
@@ -7,24 +7,23 @@ from .base import *
 
 
 DEBUG = True
-ALLOWED_HOSTS = ['localhost', '127.0.0.1']
+ALLOWED_HOSTS = ["localhost", "127.0.0.1"]
 
 # CORSのホワイトリスト
-CORS_ORIGIN_WHITELIST = ['http://localhost:3000']
-CSRF_TRUSTED_ORIGINS = ['http://localhost:3000']
 CORS_ALLOWED_ORIGINS = ["http://localhost:3000"]
 
-
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": BASE_DIR / "db.sqlite3",
     }
 }
 
 # 画像などのアップロードファイルを保存するパス(デバッグ用)
-MEDIA_ROOT = BASE_DIR / 'media'
+MEDIA_ROOT = BASE_DIR / "media"
 
 # Google認証情報
-GOOGLE_CLIENT_ID = '150637606230-jtvhjc7kspor9gls59gerknguhrhlh5j.apps.googleusercontent.com'
-SOCIAL_SECRET = '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'
+GOOGLE_CLIENT_ID = (
+    "150637606230-jtvhjc7kspor9gls59gerknguhrhlh5j.apps.googleusercontent.com"
+)
+SOCIAL_SECRET = "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"
